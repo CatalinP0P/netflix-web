d095a64da854c4d53d65f0b1e7e1ce06f68a7c2b
home <home@MacBook-Pro-home.local>
2023-07-02 14:48:04 +0300

    Added hompage and hero area

---
+++ client/src/components/InfoButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as InfoSVG } from "../assets/info.svg";
+
+export default function InfoButton() {
+  return (
+    <button className="text-white transition-all bg-transparent hover:bg-secondary flex flex-row items-center gap-1 py-[.125rem] px-2 rounded-[5px] justify-center text-sm border border-secondary">
+      <InfoSVG className="w-[30px] h-[30px] fill-white transition-all p-1" />
+      <label>More Info</label>
+      <div />
+    </button>
+  );
+}
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import heroImage from "../../assets/johnwick4.jpeg";
+import Containter from "../../components/Containter";
+import PlayButton from "../../components/PlayButton";
+import InfoButton from "../../components/InfoButton";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <img
+        className="absolute top-0 left-0 w-full h-screen object-cover z-[1]"
+        src={heroImage}
+      />
+      <div className="absolute top-0 left-0 w-full min-h-screen bg-[rgba(0,0,0,.5)] z-[2]" />
+
+      <Containter className="text-white z-[100] pt-48">
+        <div className="w-[45%] flex flex-col gap-3">
+          <label className="text-4xl font-bold z-[100]">
+            John Wick: Chapter 4
+          </label>
+          <div className="flex flex-row gap-2 z-[100] items-baseline">
+            <label className="text-green-600 font-semibold text-sm">
+              79% Match
+            </label>
+            <label className="text-muted">2023</label>
+          </div>
+
+          <p className="text-white text-sm font-thin z-[100]">
+            With the price on his head ever increasing, John Wick uncovers a
+            path to defeating The High Table. But before he can earn his
+            freedom, Wick must face off against a new enemy with powerful
+            alliances across the globe and forces that turn old friends into
+            foes.
+          </p>
+          <div className="flex flex-row gap-2 z-[100]">
+            <PlayButton />
+            <InfoButton />
+          </div>
+        </div>
+
+
+        
+      </Containter>
+    </>
+  );
 }
