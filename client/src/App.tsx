d24b719a014fdb1b866234c950ffaf139af58974
home <home@MacBook-Pro-home.local>
2023-07-02 12:29:17 +0300

    Added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "./Layout";
+import { Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
 
 function App() {
   return (
     <div>
-      <Layout></Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
